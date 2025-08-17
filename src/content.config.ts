1e939004a3bacd824a6903ab97c2df3b998fd5fe
Toby Such <toby@tobysuch.uk>
2025-08-17 23:41:32 +0100

    Add About page layout and update content configuration

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,20 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
+const about = defineCollection({
+  // Load Markdown and MDX files in the `src/content/about/` directory.
+  loader: glob({ base: "./src/content", pattern: "about.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+    }),
+});
+
 const blog = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
@@ -10,10 +24,10 @@ const blog = defineCollection({
       title: z.string(),
       description: z.string(),
       // Transform string to Date object
-      pubDate: z.coerce.date().optional(),
+      pubDate: z.coerce.date(),
       updatedDate: z.coerce.date().optional(),
       heroImage: image().optional(),
     }),
 });
 
-export const collections = { blog };
+export const collections = { about, blog };
